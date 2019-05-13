b52ca6e14fdca0f2b7bdcd80aefdfc520ef70df9
divya <divya18@navgurukul.org>
2019-05-13 16:32:29 +0530

    for loop implementation

--- BinarySearcher.java
+++ BinarySearcher.java
@@ -4,6 +4,9 @@ public class BinarySearcher {
         int [] searchNumberIn = {1,2,3,4,5,6,7,8,9,10,11,12,13};
         int highestIndex = searchNumberIn.length - 1;
         int lowestIndex = 0;
+        for (int i = 0; i < searchNumberIn.length; i++) {
+            int middileIndex = (highestIndex + lowestIndex) / 2;
 
+        }
     }
 }
