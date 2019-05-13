bc465bc90bc2c73884d55c8ebff478b10f795b5f
divya <divya18@navgurukul.org>
2019-05-13 19:12:19 +0530

    add . testcases

---
+++ test.sh
@@ -0,0 +1,8 @@
+java BinarySearcher.java 8 > actual
+diff testexpect8 actual
+java BinarySearcher.java 1 > actual
+diff testexpect1 actual
+java BinarySearcher.java 54 > actual
+diff expectNotIn actual
+java BinarySearcher.java 100 > actual
+diff expectNotIn actual
