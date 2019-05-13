d73a9baac8e7d05ba82e770723f3b829646f11fa
divya <divya18@navgurukul.org>
2019-05-13 19:04:39 +0530

    if number is greater condition

--- BinarySearcher.java
+++ BinarySearcher.java
@@ -5,13 +5,18 @@ public class BinarySearcher {
         int highestIndex = searchNumberIn.length;
         int lowestIndex = 0;
         for (int i = 0; i < searchNumberIn.length; i++) {
-            int middileIndex = (highestIndex + lowestIndex) / 2;
-            if (searchNumberIn[middileIndex] == toBeSearched) {
-                System.out.println("The number is present in the list at the index " + middileIndex);
-                return;
-            } else if (searchNumberIn[middileIndex] > toBeSearched) {
+            int middileIndex = (highestIndex+lowestIndex)/2;
+            if(searchNumberIn[middileIndex] == toBeSearched){
+               System.out.println("The number is present in the list at the index " + middileIndex);
+               return;
+           }
+           else if (searchNumberIn[middileIndex] > toBeSearched){
                 highestIndex = middileIndex;
-            }
+           }
+           else if (searchNumberIn[middileIndex] < toBeSearched){
+              lowestIndex = middileIndex;
+           }
+
         }
         System.out.println("Number not in the list");
     }
