8665618990466839aea6b9ead3c60aefbefc182b
divya <divya18@navgurukul.org>
2019-05-13 15:34:36 +0530

    binary searcher

---
+++ BinarySearcher.java
@@ -0,0 +1,6 @@
+public class BinarySearcher {
+    public static void main(String[] args) {
+        int toBeSearched = Integer.parseInt(args[0]);
+        System.out.println(toBeSearched);
+    }
+}
