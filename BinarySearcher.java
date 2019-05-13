ec6daaab21d8653daa67c03337fca5a192141dbb
divya <divya18@navgurukul.org>
2019-05-13 16:31:45 +0530

    variables defined

--- BinarySearcher.java
+++ BinarySearcher.java
@@ -1,6 +1,9 @@
 public class BinarySearcher {
     public static void main(String[] args) {
         int toBeSearched = Integer.parseInt(args[0]);
-        System.out.println(toBeSearched);
+        int [] searchNumberIn = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+        int highestIndex = searchNumberIn.length - 1;
+        int lowestIndex = 0;
+
     }
 }
