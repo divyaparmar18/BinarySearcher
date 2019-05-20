cabfcdf751640aa1f743362dedc8de8dbf2c176a
divya <divya18@navgurukul.org>
2019-05-20 13:11:40 +0530

    refactor . array name

--- test/java/searcher/searcherTest.java
+++ test/java/searcher/searcherTest.java
@@ -11,14 +11,14 @@ public class searcherTest {
     }
     @Test
     public void theIndexOfTheNumberToBeShownShouldBeGiven() {
-        int[] expected = {3,5,8,12,14,15,17,18,20};
+        int[] toBeSearchedIn = {3,5,8,12,14,15,17,18,20};
         String obj = "The number is present in the list at the index 3";
-        assertEquals(obj, BinarySearcher.getNumberFinder(12, expected, 9, 0));
+        assertEquals(obj, BinarySearcher.getNumberFinder(12, toBeSearchedIn, 9, 0));
     }
     @Test
     public void ifTheNumberIsNotInTheArrayItShouldSayNotInTheArray() {
-        int[] expected = {3,5,8,12,14,15,17,18,20};
+        int[] t0BeSearchedIn = {3,5,8,12,14,15,17,18,20};
         String obj = "Number not in the list";
-        assertEquals(obj, BinarySearcher.getNumberFinder(29, expected, 9, 0));
+        assertEquals(obj, BinarySearcher.getNumberFinder(29, t0BeSearchedIn, 9, 0));
     }
 }
