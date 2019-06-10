d1469cee19baf830b39c6718b520c5449351aefd
divya <divya18@navgurukul.org>
2019-06-10 13:43:45 +0530

    refactor int type to generic

--- main/java/searcher/BinarySearcher.java
+++ main/java/searcher/BinarySearcher.java
@@ -2,24 +2,23 @@ package searcher;
 
 public class BinarySearcher {
     public static void main(String[] args) {
-        int toBeSearched = Integer.parseInt(args[0]);
-        int[] searchNumberIn = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int toBeSearched = 9;
+        Integer[] searchNumberIn = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int highestIndex = searchNumberIn.length, lowestIndex = 0;
-        System.out.println(getNumberFinder(toBeSearched, searchNumberIn, highestIndex, lowestIndex));
+        System.out.println(getNumberFinder(toBeSearched, searchNumberIn,highestIndex, lowestIndex));
     }
 
-    public static int getNumberFinder(int toBeSearched, int[] searchNumberIn, int highestIndex, int lowestIndex) {
+    public static <T extends Comparable<T>>int getNumberFinder(T toBeSearched, T[] searchNumberIn, int highestIndex, int lowestIndex) {
         if(lowestIndex < searchNumberIn.length-1) {
             int middileIndex = getMiddileIndex(highestIndex, lowestIndex);
             if (searchNumberIn[middileIndex] == toBeSearched) {
                 return ( middileIndex);
-            } else if (searchNumberIn[middileIndex] > toBeSearched) {
+            } else if (searchNumberIn[middileIndex].compareTo(toBeSearched)>0) {
                 return getNumberFinder(toBeSearched, searchNumberIn, middileIndex, lowestIndex);
-            } else if (searchNumberIn[middileIndex] < toBeSearched) {
+            } else if (searchNumberIn[middileIndex].compareTo(toBeSearched)<0) {
                 return getNumberFinder(toBeSearched, searchNumberIn, highestIndex, middileIndex);
             }
         }
-
         return (-1);
     }
 
