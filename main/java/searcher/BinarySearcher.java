33adfa57b768a75e2e9ab6501759144d47b3cf54
divya <divya18@navgurukul.org>
2019-06-10 14:08:54 +0530

    try with strings

--- main/java/searcher/BinarySearcher.java
+++ main/java/searcher/BinarySearcher.java
@@ -2,29 +2,33 @@ package searcher;
 
 public class BinarySearcher {
     public static void main(String[] args) {
-        int toBeSearched = 9;
-        Integer[] searchNumberIn = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int toBeSearched = 1;
+        String name = "shinuu";
+        Integer[] searchNumberIn = {1, 2, 3, 4};
+        String[] names = {"akshay","divya","shinuu","zoyaa"};
+
         int highestIndex = searchNumberIn.length, lowestIndex = 0;
-        System.out.println(getNumberFinder(toBeSearched, searchNumberIn,highestIndex, lowestIndex));
+        System.out.println(indexFinder(toBeSearched, searchNumberIn, highestIndex, lowestIndex));
+        System.out.println(indexFinder(name, names, names.length, lowestIndex));
     }
 
-    public static <T extends Comparable<T>>int getNumberFinder(T toBeSearched, T[] searchNumberIn, int highestIndex, int lowestIndex) {
-        if(lowestIndex < searchNumberIn.length-1) {
+    public static <T extends Comparable<T>> int indexFinder(T toBeSearched, T[] searchNumberIn, int highestIndex, int lowestIndex) {
+        if (lowestIndex < searchNumberIn.length - 1) {
             int middileIndex = getMiddileIndex(highestIndex, lowestIndex);
             if (searchNumberIn[middileIndex] == toBeSearched) {
-                return ( middileIndex);
-            } else if (searchNumberIn[middileIndex].compareTo(toBeSearched)>0) {
-                return getNumberFinder(toBeSearched, searchNumberIn, middileIndex, lowestIndex);
-            } else if (searchNumberIn[middileIndex].compareTo(toBeSearched)<0) {
-                return getNumberFinder(toBeSearched, searchNumberIn, highestIndex, middileIndex);
+                return (middileIndex);
+            } else if (searchNumberIn[middileIndex].compareTo(toBeSearched) > 0) {
+                return indexFinder(toBeSearched, searchNumberIn, middileIndex, lowestIndex);
+            } else if (searchNumberIn[middileIndex].compareTo(toBeSearched) < 0) {
+                return indexFinder(toBeSearched, searchNumberIn, highestIndex, middileIndex);
             }
         }
         return (-1);
     }
 
     private static int getMiddileIndex(int highestIndex, int lowestIndex) {
-        return (highestIndex+lowestIndex)/2;
+        return (highestIndex + lowestIndex) / 2;
     }
 
-    }
+}
 
--- test/java/searcher/searcherTest.java
+++ test/java/searcher/searcherTest.java
@@ -5,17 +5,17 @@ import static org.junit.Assert.assertEquals;
 public class searcherTest {
     @Test
     public void theIndexofTheNumberShouldBeGiven() {
-        int[] toBeSearchedIn = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        assertEquals(5, BinarySearcher.getNumberFinder(6, toBeSearchedIn, 11, 0));
+        Integer[] toBeSearchedIn = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        assertEquals(5, BinarySearcher.indexFinder(6, toBeSearchedIn, 11, 0));
     }
     @Test
     public void theIndexOfTheNumberToBeSearchedShouldBeGiven() {
-        int[] toBeSearchedIn = {3,5,8,12,14,15,17,18,20};
-        assertEquals(3, BinarySearcher.getNumberFinder(12, toBeSearchedIn, 9, 0));
+        Integer[] toBeSearchedIn = {3,5,8,12,14,15,17,18,20};
+        assertEquals(3, BinarySearcher.indexFinder(12, toBeSearchedIn, 9, 0));
     }
     @Test
     public void ifTheNumberIsNotInTheArray() {
-        int[] t0BeSearchedIn = {3,5,8,12,14,15,17,18,20};
-        assertEquals(-1, BinarySearcher.getNumberFinder(29, t0BeSearchedIn, 9, 0));
+        Integer[] t0BeSearchedIn = {3,5,8,12,14,15,17,18,20};
+        assertEquals(-1, BinarySearcher.indexFinder(29, t0BeSearchedIn, 9, 0));
     }
 }
