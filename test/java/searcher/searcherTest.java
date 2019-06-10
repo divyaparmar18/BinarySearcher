e1f4435f7b2efb4415cefd1125b317ce5a6240c9
divya <divya18@navgurukul.org>
2019-06-10 14:11:23 +0530

    add testcases for String

--- test/java/searcher/searcherTest.java
+++ test/java/searcher/searcherTest.java
@@ -18,4 +18,9 @@ public class searcherTest {
         Integer[] t0BeSearchedIn = {3,5,8,12,14,15,17,18,20};
         assertEquals(-1, BinarySearcher.indexFinder(29, t0BeSearchedIn, 9, 0));
     }
+    @Test
+    public void ifTheNameIsNotInTheArray() {
+        String[] t0BeSearchedIn = {"divya","gauri","kajal","priya","zoyaa"};
+        assertEquals(4, BinarySearcher.indexFinder("zoyaa", t0BeSearchedIn, 5, 0));
+    }
 }
